665272005c9b9257b02a8c73e57ab928039cae54
CharlesSouzaAlmeida <68315827+CharlesSouzaAlmeida@users.noreply.github.com>
2022-02-18 20:25:54 -0300

    Add files via upload

---
+++ desafio-cap-java/desafio-cap-java/README.md
@@ -0,0 +1,4 @@
+Desafio de programação Capgemini
+Baixe o Projeto e execute na IDE de sua preferencia
+Siga o caminho: src/main/java/br/com/proway/capgemini
+entre na Classe Program.Java e Siga as orientações comentadas.
---
+++ desafio-cap-java/desafio-cap-java/src/main/java/br/com/proway/capgemini/Program.java
@@ -0,0 +1,32 @@
+package br.com.proway.capgemini;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        Desafio charles = new Desafio();
+        charles.questaoUm(9);
+        charles.questaoDois("Charles@123");
+        charles.questaoTres("CharlesDesafioCap");
+
+
+
+        /**
+         * Instanciar a Class Desafio
+         * exemplo: Desafio desagio = new Desafio();
+         * desafio.questaoUm(Passar o numero)
+         * */
+
+
+        /**
+         * Chamar método questaoDois, e passar uma String no parametro para que ele verifique a força da senha
+         * exemplo: desafio.questaoDois("Samuel20");
+         * */
+
+
+        /**
+         * Chamar método questaoTres, e passar uma String no parametro para que ele conte seus pares de Anagramas
+         * exemplo: desafio.questaoTres("samuel2022capgemini");
+         * */
+    }
+}
